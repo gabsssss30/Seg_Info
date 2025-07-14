13687510f5fdf3b53b75a554a8485a154e3ff38e
gabsssss30 <gfl30052006@gmail.com>
2025-07-14 22:03:34 +0000

    Pending changes exported from your codespace

--- app.py
+++ app.py
@@ -2,6 +2,19 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route("/")
-def hello_world():
-    return render_template("index.html", title="Hello")
+@app.route('/')
+def home():
+    return render_template('home.html')
+
+# Rota pública para mostrar informações auto
+@app.route('/informacoesauto')
+def informacoes_auto():
+    return render_template('auto.html')
+
+# Rota pública para mostrar informações emp
+@app.route('/informacoesemp')
+def informacoes_emp():
+    return render_template('emp.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
